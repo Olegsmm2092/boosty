9847c38ad26c6b606d5de0232ca27cb1be86be75
Olegsmm2092 <radoveev@gmail.com>
2025-04-03 07:55:27 +0800

    fix: match regex validation to Enum in models/ticket.py

--- app/models/ticket.py
+++ app/models/ticket.py
@@ -1,17 +1,23 @@
+from enum import Enum
 from pydantic import BaseModel, Field, validator
 import re
 
+class BugType(str, Enum):
+    Bug = 'Bug'
+    Story = 'Story'
+    Epic = 'Epic'
+
+class StatusType(str, Enum):
+    Draft = 'Draft'
+    Published = 'Published'
+    InProgress = 'In progress'
+
 class TicketModel(BaseModel):
     id: int
-    title: str|None = Field(max_length=30, default=None)
-    status: str = "Draft"
-    type: str = Field(default='Bug', pattern=r'^Bug|Story|Epic$')
+    title: str|None = Field(max_length=30, default=None, alias='Title')
+    status: StatusType = StatusType.Draft
+    type: BugType = BugType.Bug
     severity: int = Field(default=0, ge=0, lt=10)
 
-    @validator("type")
-    def validate_type(cls, value):
-        duplicate_last_letter_pattern = r'(\w)\1$'  # Checks if last letter is duplicated
-        if re.search(duplicate_last_letter_pattern, value):
-            raise ValueError(f"'{value}' contains a duplicate last letter.")
-        return value
+
 
